7d195b1a00105e9375db6234839821485bb67833
boonecabaldev <164784238+boonecabaldev@users.noreply.github.com>
2024-06-09 20:51:57 +0000

    adding sytles to books to make easier to read

--- bookscraper/bookscraper/spiders/books.py
+++ bookscraper/bookscraper/spiders/books.py
@@ -10,19 +10,19 @@ class BooksSpider(scrapy.Spider):
         # Wasp Factory
         "https://bookreadfree.com/book/132266"
         # Porno For Psychos
-        #, "https://bookreadfree.com/book/242741"
+        , "https://bookreadfree.com/book/242741"
         # Tetatologist
-        #, "https://bookreadfree.com/book/251503"
+        , "https://bookreadfree.com/book/251503"
         # The Black Train
-        #, "https://bookreadfree.com/book/426333"
+        , "https://bookreadfree.com/book/426333"
         # Mangled Meat
-        #, "https://bookreadfree.com/book/497228"
+        , "https://bookreadfree.com/book/497228"
         # Terra Insanus
-        #, "https://bookreadfree.com/book/195883"
+        , "https://bookreadfree.com/book/195883"
         # Bullet Through Your Face
-        #, "https://bookreadfree.com/book/371136"
+        , "https://bookreadfree.com/book/371136"
         # "The Haunter Of The Threshold"
-        #, "https://bookreadfree.com/book/477396"
+        , "https://bookreadfree.com/book/477396"
         # Header 2
         , "https://bookreadfree.com/book/511045"
         ]
@@ -45,6 +45,7 @@ class BooksSpider(scrapy.Spider):
         first_p = response.xpath("//article[@class='c']/div[@class='con'][2]/p[1]").extract_first().strip().replace('\xa0', '')
         if first_p:
             first_p = '<p>' + re.sub('<br><br>', '</p><p>', str(first_p)) + '</p>'
+            html = f"<body style='color: #b1b9c7; background-color: black; max-width: 650px; margin-left: auto; margin-right: auto; font: 1.05rem Arial'>{str(first_p)}</body>"
 
             dir_name = f"../pages/{title}"
             if not os.path.exists(dir_name):
@@ -53,4 +54,4 @@ class BooksSpider(scrapy.Spider):
             html_filename = f"{dir_name}/{title}_page{count}.html"
             print("HTML Filename: ", html_filename)
             with open(html_filename, 'w') as f:
-                f.write(str(first_p))
+                f.write(html)
